ac0b88214f442ccc4cc5fffefbdfb98eface8d5f
enzdor <enzo01lb@gmail.com>
2025-04-12 01:08:42 -0300

    create script to clean csi data

---
+++ clean-csi.py
@@ -0,0 +1,84 @@
+import numpy as np
+import datetime as dt
+import pandas as pd
+import sqlite3
+import sys
+import argparse
+import pickle
+import gc
+from io import StringIO
+
+#################################################
+
+
+    # parsing flags and opening csv
+
+
+#################################################
+
+parser = argparse.ArgumentParser(description="""
+        python clean-csi.py [-h] cpi.csv
+
+        Clean raw csi monthly data to then use in csi-cpi.py and predict.py.
+        Link for the data:
+
+        cpi: https://fred.stlouisfed.org/series/CPIAUCSL
+
+""")
+
+parser.add_argument("csi_path", help="""
+        Path to the file containing raw csi montly data from the surveys of
+        consumers by the University of Michigan. Link to download raw
+        data: https://fred.stlouisfed.org/series/CPIAUCSL .
+""")
+parser.add_argument("-o", "--outfile", dest="outfile", default="csi_clean.csv", help="""
+        The path for the outfile, the clean csi data.
+""")
+
+args = parser.parse_args()
+
+if len(sys.argv) < 2:
+    print("""
+        There was no path provided for the raw csi monthly data.
+        Corect usage:
+            
+            python clean-csi.py path_to_raw_csi.csv
+            
+        If you want help run:
+            
+            python clean-csi.py --help
+    """)
+    quit()
+
+if len(args.csi_path) < 1:
+    print(f"""
+        There was not path provided for the raw csi monthly data.
+        Path provided, csi:{args.csi_path} .
+            
+            python cpi-csi.py path_to_cpi.csv path_to_csi.csv
+            
+        If you want help run:
+            
+            python cpi-csi.py --help
+    """)
+    quit()
+
+file_csi = open(args.csi_path)
+lines = file_csi.readlines()
+lines = lines[1:]
+new_lines = []
+
+for l in lines:
+    new_lines.append(l[:len(l)-2] + l[len(l)-1])
+    
+final_line = ""
+
+for l in new_lines:
+    final_line += l
+
+io_dirty = StringIO(final_line)
+
+df_dirty = pd.read_csv(io_dirty)
+df_dirty['date'] = [dt.date(x, y, 1) for x, y in zip(df_dirty['Year'], df_dirty['Month'])]
+
+df_dirty.to_csv(args.outfile, columns = ['Index', 'date'], index = False)
--- cpi-csi.py
+++ cpi-csi.py
@@ -18,9 +18,9 @@ import gc
 parser = argparse.ArgumentParser(description="""
         python predict.py [-h] cpi.csv csi.csv 
 
-        Predict future consumer price index based on the consumer 
-        sentiment index from the University of Michingan using
-        FLAML's automl. Links for the data:
+        Create a model to predict future consumer price index 
+        based on the consumer sentiment index from the University of 
+        Michingan using FLAML's automl. Links for the monthly data:
 
         csi: https://data.sca.isr.umich.edu/data-archive/mine.php
         cpi: https://fred.stlouisfed.org/series/CPIAUCSL
@@ -28,10 +28,10 @@ parser = argparse.ArgumentParser(description="""
 """)
 
 parser.add_argument("cpi_path", help="""
-        Path to the file containing cpi data from FRED.
+        Path to the file containing monthly cpi data from FRED.
 """)
 parser.add_argument("csi_path", help="""
-        Path to the file containing csi data from the surveys of
+        Path to the file containing monthly csi data from the surveys of
         consumers by the University of Michigan.
 """)
 parser.add_argument("-o", "--outfile", dest="outfile", default="model.pkl", help="""
@@ -39,3 +39,44 @@ parser.add_argument("-o", "--outfile", dest="outfile", default="model.pkl", help
 """)
 
 args = parser.parse_args()
+
+if len(sys.argv) < 3:
+    print("""
+        There was no path provided for the monthly cpi and csi data.
+        Corect usage:
+            
+            python cpi-csi.py path_to_cpi.csv path_to_csi.csv
+            
+        If you want help run:
+            
+            python cpi-csi.py --help
+    """)
+    quit()
+
+if len(args.cpi_path) < 1 or len(args.csi_path) < 1:
+    print(f"""
+        There was not path provided for the monthly cpi or csi data.
+        Paths provided, cpi: {args.cpi_path} , csi:{args.csi_path} .
+
+            python cpi-csi.py path_to_cpi.csv path_to_csi.csv
+            
+        If you want help run:
+            
+            python cpi-csi.py --help
+    """)
+    quit()
+
+print(f"[{dt.datetime.now()}] Loading cpi data from {args.cpi_path}")
+df_cpi = pd.read_csv(args.cpi_path)
+print(f"[{dt.datetime.now()}] Loading csi data from {args.csi_path}")
+df_csi = pd.read_csv(args.csi_path)
+
+if len(df_cpi) < 1:
+    print("cpi csv doesn't contain any rows.")
+    quit()
+
+if len(df_csi) < 1:
+    print("csi csv doesn't contain any rows.")
+    quit()
+
+print(df_cpi[:3], df_csi[:3])
